a934d5fc3984eff5780bf9d849931507ae55ef71
triinutriinu <trparn@ttu.ee>
2022-05-26 12:43:30 +0300

    exam

--- ping.py
+++ ping.py
@@ -1,8 +1,9 @@
 import requests
 
-def check_server_status():
+
+def check_server_status(url):
     a = requests.get(url)
-    if a = 200:
+    if a.status_code == 200:
         return True
     else:
         return False
